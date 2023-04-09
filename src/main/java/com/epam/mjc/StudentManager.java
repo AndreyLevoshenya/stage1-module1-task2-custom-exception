61e42eae75d7ddf3cc3385ccf08bdd311facddab
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-04-10 01:13:15 +0300

    Add files via upload

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,16 +6,23 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+      if(Student.getValueOf(studentID) == null) {
+          throw new StudentNotFoundException(studentID);
+      }
+      return Student.getValueOf(studentID);
   }
 
   public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
+      StudentManager manager = new StudentManager();
 
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
-    }
+      for (int i = 0; i < IDs.length; i++) {
+          try {
+            Student student = manager.find(IDs[i]);
+            System.out.println("Student name " + student.getName());
+          } catch (StudentNotFoundException e) {
+              System.out.println(e.getMessage());
+          }
 
+      }
   }
 }
